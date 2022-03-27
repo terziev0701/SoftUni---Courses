344c4cc5d4332874a7ccbf0b9a2347c5cf00e9d6
Valentin Terziev <valentinterziev9701@gmail.com>
2022-03-27 13:57:48 +0300

    Add files via upload

---
+++ TradeCommissions.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class TradeCommissions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String town = scanner.nextLine();
+        double sales = Double.parseDouble(scanner.nextLine());
+
+        double commission = 0;
+        boolean isValid = true;
+        if (town.equals("Sofia")) {
+            if (sales >= 0 && sales <= 500) {
+                commission = sales * 0.05;
+            } else if (sales > 500 && sales <= 1000) {
+                commission = sales * 0.07;
+            } else if (sales > 1000 && sales <= 10000) {
+                commission = sales * 0.08;
+            } else if (sales > 10000) {
+                commission = sales * 0.12;
+            } else {
+                isValid = false;
+            }
+        } else if (town.equals("Varna")) {
+            if (sales >= 0 && sales <= 500) {
+                commission = sales * 0.045;
+            } else if (sales > 500 && sales <= 1000) {
+                commission = sales * 0.075;
+            } else if (sales > 1000 && sales <= 10000) {
+                commission = sales * 0.10;
+            } else if (sales > 10000) {
+                commission = sales * 0.13;
+            } else {
+                isValid = false;
+            }
+        } else if (town.equals("Plovdiv")) {
+            if (sales >= 0 && sales <= 500) {
+                commission = sales * 0.055;
+            } else if (sales > 500 && sales <= 1000) {
+                commission = sales * 0.08;
+            } else if (sales > 1000 && sales <= 10000) {
+                commission = sales * 0.12;
+            } else if (sales > 10000) {
+                commission = sales * 0.145;
+            } else {
+                isValid = false;
+            }
+        } else {
+            isValid = false;
+        }
+
+        if (!isValid){
+            System.out.println("error");
+        } else {
+            System.out.printf("%.2f", commission);
+        }
+    }
+}
